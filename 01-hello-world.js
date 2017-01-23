14da09a043c821fbd4912339f2a4eb7103f2484d
Matt Lewis <matthew.lewis@pagesuite.com>
2017-01-23 10:39:46 +0000

    initial commit

---
+++ 01-hello-world.js
@@ -0,0 +1,17 @@
+/*
+    Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
+
+    The port number will be provided to you by expressworks as the first argument of
+    the application, i.e., process.argv[2].
+*/
+
+const express = require('express');
+const app = express();
+
+const port = process.argv[2];
+
+app.get('/home', (req, res) => {
+    res.end('Hello World!');
+});
+
+app.listen(port);
